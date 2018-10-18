52e5b85b115e41323371abafbb41d8344011005a
agabriel1 <43051089+agabriel1@users.noreply.github.com>
2018-10-18 00:03:49 -0400

    Delete mpi_path.sh

--- mpi_path.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "export PATH='$PATH:/software/bin'" >> /users/AG899460/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/AG899460/.bashrc
-source /users/AG899460/.bashrc
